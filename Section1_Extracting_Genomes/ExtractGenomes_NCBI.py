86e5d46dcebe04010f9099434c255d68ec8393ec
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2020-03-02 17:48:43 +0000

    Move Entrez eFetch handel to a function

--- Section1_Extracting_Genomes/ExtractGenomes_NCBI.py
+++ Section1_Extracting_Genomes/ExtractGenomes_NCBI.py
@@ -4,10 +4,34 @@
 from Bio import Entrez
 Entrez.email = eemh1@st-andrews.ac.uk
 
-def extractAssembly(database, ID, rettype_argmnt, retmode_argmnt)
-handel = Entrez.efetch(db = database, id = ID, rettype = rettype_argmnt, retmode = retmode_argmnt)
-print(handle.read())
-# add code to print out each pulled down genome to a new text file, named after it's ID with a standard name formal as well
+
+# Create function to continually access desired NCBI database, and pull down data in desired output format.
+def extractAssembly(database, ID, rettype_argmnt, retmode_argmnt):
+    """This function pulls down the data from the specied NCBI database in the specified format.
+    For information about Entrez.BioPython module's keywords for rettype and retmode (which define
+    the output format of the pulled down data, see:
+    https://biopython.org/DIST/docs/api/Bio.Entrez-module.html
+    https://biopython.org/DIST/docs/tutorial/Tutorial.html#htoc111 >> Chapter 9
+    This function uses the Entrez eFetch handel which retrieves data from NCBI, for more information see:
+    https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.EFetch
+    """
+    
+    handel = Entrez.efetch(db=database, id=ID, rettype=rettype_argmnt, retmode=retmode_argmnt)
+    print(handel.read())
+    
+# Define function to extract accession numbers of datasets of interest in NCBI database.
+# The accession numbers must be stored in a plain text file in the same folder as this script.
+# Additionally, the plain text file must contain no blank/empty lines and only the accession numbers.
+def getAccessionNumbers(accessionNumbersFile):
+    
+    
+# add code to print out each pulled down genome to a new text file, named after it's ID with a standard name
+# formal as well
+
+# Extract 
+
+
+
 
 
 # The arguments rettype="gb" and retmode="text" let us download this record in the GenBank format.
