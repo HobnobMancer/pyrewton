eb2924c70f0098151643618b67626d04dd060eff
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2020-03-06 22:48:53 +0000

    Correct docstring formatting

--- Section1_Extracting_Genomes/Extract_genomes_NCBI.py
+++ Section1_Extracting_Genomes/Extract_genomes_NCBI.py
@@ -16,7 +16,9 @@ NCBI_database = 'NCBIAssembly' # name the database from which data will be pulle
 
 # Create function to continually access desired NCBI database, and pull down data in desired output format.
 def extract_assembly(accession_number):
-    """This function pulls down the data from the specified NCBI database in the specified format.
+    """The function extract_assebly pulls down genomic sequences from the NCBI assembly database.
+    
+    This function pulls down the data from the specified NCBI database in the specified format.
     For information about Entrez.BioPython module's keywords for rettype and retmode (which define
     the output format of the pulled down data, see:
     https://biopython.org/DIST/docs/api/Bio.Entrez-module.html
@@ -35,7 +37,9 @@ def extract_assembly(accession_number):
 
 # Define function to extract accession numbers of datasets of interest in NCBI database.
 def get_accession_numbers(accession_numbers_file):
-    """Extracts the accession numbers from the 'inputFILE' containing the accession numbers.
+    """The get_accession_numbers function extracts accession numbers from a plain text file
+
+    This function extract the accession numbers of genomic sequences stored in the NCBI assembly database.
     The input file must be a plain text file in the same folder as this script.
     Additionally, the input file must not contain any blank/empty lines and only contain the accession numbers.
     The accession numbers will be returned as a list with each number being assigned an individual element"""
@@ -46,15 +50,21 @@ def get_accession_numbers(accession_numbers_file):
     input_file.close()
     return(accesssion_numbers)
 
+    with open("fname", "r") as fhandle:
+    fhandle.read()
+
 
 # Define function to store pulled down data from NCBI in a individual data file for that associated accession number
 def store_ncbi_data(working_accession_number, NCBI_data):
-    """This function stores the data pulled down from the NCBI database in a new file, named in the predefined manner.
+    """ The store_ncbi_data function stores data pulled down from NCBI in a new file.
+
+    This function stores the data pulled down from a NCBI database in a new file, named in the predefined manner.
+    The naming manner/system is defined in this function.
     File naming system: 'data-of-pulldown_Accession#_NCBI-database-name.
     Example file name: 02-03-2020_GCA00014548_NCBIAssembly
     The file extension will also be defined using this function.
-    The created output file will be stored in the same directory as this scritp"""
-    # date-of-pulldown  and NCBIdatabase (the database from which data will be pulled) are defined at the top the script
+    The created output file will be stored in the same directory as this scritp
+    data_of_pulldown and NCBI_database must be defined at the beginning of the main script"""
     data_accession_number = NCBI_data # the accession number of the NCBI data of interest is passed to the function
 
     file_format = '.txt' # define the file extention for the output file
