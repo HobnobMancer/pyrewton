ae5c4a93f6a9bc0131eb7a7e261e682568f5e42a
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2020-03-06 22:33:18 +0000

    Correct variable/function name formatting

--- Section1_Extracting_Genomes/Extract_genomes_NCBI.py
+++ Section1_Extracting_Genomes/Extract_genomes_NCBI.py
@@ -9,13 +9,13 @@ from Bio import Entrez
 # Fill out pull down form:
 Entrez.email = eemh1@st-andrews.ac.uk # enter email of person performing the pulldown
 date_of_pulldown = '03-03-2020' # enter the start date of executing this script, as a string
-NCBIdatabase = 'NCBIAssembly' # name the database from which data will be pulled from, as a string
+NCBI_database = 'NCBIAssembly' # name the database from which data will be pulled from, as a string
 
 
 
 
 # Create function to continually access desired NCBI database, and pull down data in desired output format.
-def extractAssembly(accessionNumber):
+def extract_assembly(accession_number):
     """This function pulls down the data from the specified NCBI database in the specified format.
     For information about Entrez.BioPython module's keywords for rettype and retmode (which define
     the output format of the pulled down data, see:
@@ -28,41 +28,41 @@ def extractAssembly(accessionNumber):
     rettype_argmnt = "gb"
     retmode_argmnt = "text"
 
-    handel = Entrez.efetch(db=NCBIdatabase, id=accessionNumber, rettype=rettype_argmnt, retmode=retmode_argmnt)
+    handel = Entrez.efetch(db=NCBI_database, id=accession_number, rettype=rettype_argmnt, retmode=retmode_argmnt)
     print(handel.read())
     
 
 
 # Define function to extract accession numbers of datasets of interest in NCBI database.
-def getAccessionNumbers(accessionNumbersFile):
+def get_accession_numbers(accession_numbers_file):
     """Extracts the accession numbers from the 'inputFILE' containing the accession numbers.
     The input file must be a plain text file in the same folder as this script.
     Additionally, the input file must not contain any blank/empty lines and only contain the accession numbers.
     The accession numbers will be returned as a list with each number being assigned an individual element"""
 
     # inputFILE = open(r" - path for file containing accession numbers - ")
-    inputFILE = open(r"/home/em/PhD_Project_Scripts/Section_1_Extracting_Genomes/NCBI_AccessionNumbers_inputFILE.txt", "rt")
-    accesssionNumbers = inputFILE.readlines()
-    inputFILE.close()
-    return(accesssionNumbers)
+    input_file = open(r"/home/em/PhD_Project_Scripts/Section_1_Extracting_Genomes/NCBI_AccessionNumbers_inputFILE.txt", "rt")
+    accesssion_numbers = input_file.readlines()
+    input_file.close()
+    return(accesssion_numbers)
 
 
 # Define function to store pulled down data from NCBI in a individual data file for that associated accession number
-def storeNCBIdata(workingAccessionNumber, NCBIdata):
+def store_ncbi_data(working_accession_number, NCBI_data):
     """This function stores the data pulled down from the NCBI database in a new file, named in the predefined manner.
     File naming system: 'data-of-pulldown_Accession#_NCBI-database-name.
     Example file name: 02-03-2020_GCA00014548_NCBIAssembly
     The file extension will also be defined using this function.
     The created output file will be stored in the same directory as this scritp"""
     # date-of-pulldown  and NCBIdatabase (the database from which data will be pulled) are defined at the top the script
-    dataAccessionNumber = NCBIdata # the accession number of the NCBI data of interest is passed to the function
+    data_accession_number = NCBI_data # the accession number of the NCBI data of interest is passed to the function
 
-    fileFORMAT = '.txt' # define the file extention for the output file
-    fileNAME = date_of_pulldown + '_' + workingAccessionNumber + '_' + NCBIdatabase + fileFORMAT
+    file_format = '.txt' # define the file extention for the output file
+    file_name = date_of_pulldown + '_' + working_accession_number + '_' + NCBI_database + file_format
 
-    outputFILE = open(fileNAME, "w")    # create file
-    outputFILE.write(NCBIdata)          # write out pulled down data
-    outputFILE.close()                  # close file
+    output_file = open(file_name, "w")    # create file
+    output_file.write(NCBI_data)          # write out pulled down data
+    output_file.close()                  # close file
 
 
 
