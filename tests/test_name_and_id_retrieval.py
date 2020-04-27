42ae6eda315ef53581bf91fc18d881bc3348c20a
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-04-27 15:01:32 +0100

    exand code draft

--- tests/test_name_and_id_retrieval.py
+++ tests/test_name_and_id_retrieval.py
@@ -1,30 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging
+import unittest
+
 import pytest
 
-# Establish inputs for tests and expected outputs
+class TestName_and_IDRetrieval(unittest.TestCase):
 
-@pytest.fixture
-def tax_id():
-  id = 5061
+  """Class defining tests of Extract_genomes_NCBI.py name
+  and taxonomy ID retrieval. """
+
+  # Establish inputs for tests and expected outputs
+
+  @pytest.fixture
+  def tax_id():
+    id = 5061
+    return (id)
   
-@pytest.fixture
-def expected_name():
-  e_name = "Aspergillus niger"
-
-@pytest.fixture
-def species_name():
-  name = "Aspergillus nidulans"
+  @pytest.fixture
+  def expected_name():
+    e_name = "Aspergillus niger"
+    return (e_name)  
   
-@pytest.fixture
-def expected_id():
-  e_id = 162425
+  @pytest.fixture
+  def species_name():
+    name = "Aspergillus nidulans"
+    return (name)
+    
+  @pytest.fixture
+  def expected_id():
+    e_id = 162425
+    return (e_id)
   
-# Define tests
-def test_species_name_retrieval(tax_id, expected_name):
-"""Tests Entrez call to NCBI to retrieve scientific name from taxonomy ID"""
+  # Null logger instance
+  self.logger = logging.getLogger("Test_name_and_ID_Retrieval logger")
+  self.logger.addHandler(logging.NullHandler())
+  
+  # Define tests
 
-def test_taxonomy_id_retrieval(species_name, expected_id):
-"""Tests Entrez call to NCBI to retrieve taxonomy ID from scientific name"""
+  def test_species_name_retrieval(self, tax_id, expected_name):
+    """Tests Entrez call to NCBI to retrieve scientific name from taxonomy ID."""
 
+    # 0 represents an arbitary line numbers passed to the function
+    assert expected_name == Section1_Extracting_Genomes.Extract_genomes_NCBI:get_genus_species_name(tax_id, self.logger, 0)
+
+  def test_taxonomy_id_retrieval(self, species_name, expected_id):
+    """Tests Entrez call to NCBI to retrieve taxonomy ID from scientific name."""
+
+    # 0 represents an arbitary line numbers passed to the function
+    assert expected_id == Section1_Extracting_Genomes.Extract_genomes_NCBI: get_tax_ID(species_name, self.logger, 0)
+
+if __name__ == '__main__'
+  unittest.main()
