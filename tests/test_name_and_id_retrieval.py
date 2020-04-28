62a183d5fd5005472f92befc3106cd09e40a7c92
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-04-28 20:27:31 +0100

    remove fixture, add raw strings for asserts

--- tests/test_name_and_id_retrieval.py
+++ tests/test_name_and_id_retrieval.py
@@ -4,52 +4,44 @@
 import logging
 import unittest
 
+from Bio import Entrez
 import pytest
+
+from Section1_Extracting_Genomes import Extract_genomes_NCBI
+
+Entrez.email = "eemh1@standrews.ac.uk"
+
 
 class TestName_and_IDRetrieval(unittest.TestCase):
 
-  """Class defining tests of Extract_genomes_NCBI.py name
+    """Class defining tests of Extract_genomes_NCBI.py name
   and taxonomy ID retrieval. """
 
-  # Establish inputs for tests and expected outputs
+    # Establish inputs for tests and expected outputs
 
-  @pytest.fixture
-  def tax_id():
-    id = 5061
-    return (id)
-  
-  @pytest.fixture
-  def expected_name():
-    e_name = "Aspergillus niger"
-    return (e_name)  
-  
-  @pytest.fixture
-  def species_name():
-    name = "Aspergillus nidulans"
-    return (name)
-    
-  @pytest.fixture
-  def expected_id():
-    e_id = 162425
-    return (e_id)
-  
-  # Null logger instance
-  self.logger = logging.getLogger("Test_name_and_ID_Retrieval logger")
-  self.logger.addHandler(logging.NullHandler())
-  
-  # Define tests
+    def setUp(self):
+        """"Null logger instance"""
+        self.logger = logging.getLogger("Test_name_and_ID_Retrieval logger")
+        self.logger.addHandler(logging.NullHandler())
 
-  def test_species_name_retrieval(self, tax_id, expected_name):
-    """Tests Entrez call to NCBI to retrieve scientific name from taxonomy ID."""
+    # Define tests
 
-    # 0 represents an arbitary line numbers passed to the function
-    assert expected_name == Section1_Extracting_Genomes.Extract_genomes_NCBI:get_genus_species_name(tax_id, self.logger, 0)
+    def test_species_name_retrieval(self):
+        """Tests Entrez call to NCBI to retrieve scientific name from taxonomy ID."""
 
-  def test_taxonomy_id_retrieval(self, species_name, expected_id):
-    """Tests Entrez call to NCBI to retrieve taxonomy ID from scientific name."""
+        # Input for Entrez call: '5061'
+        # Expected return from call: 'Aspergillus niger'
+        # 0 represents an arbitary line numbers passed to the function
+        assert "Aspergillus niger" == Extract_genomes_NCBI.get_genus_species_name(
+            "5061", self.logger, 0
+        )
 
-    # 0 represents an arbitary line numbers passed to the function
-    assert expected_id == Section1_Extracting_Genomes.Extract_genomes_NCBI: get_tax_ID(species_name, self.logger, 0)
+    def test_taxonomy_id_retrieval(self):
+        """Tests Entrez call to NCBI to retrieve taxonomy ID from scientific name."""
 
-if __name__ == '__main__'
-  unittest.main()
+        # Input for Entrez call: 'Aspergillus nidulans'
+        # Expected return from call: '162425'
+        # 0 represents an arbitary line numbers passed to the function
+        assert "NCBI:txid162425" == Extract_genomes_NCBI.get_tax_ID(
+            "Aspergillus nidulans", self.logger, 0
+        )
