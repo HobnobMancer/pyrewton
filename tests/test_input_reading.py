20bb9d8c23c9fb7d8853765a2aea1d0fd2ea36bb
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-05-14 21:07:36 +0100

    add 'retries' argument

--- tests/test_input_reading.py
+++ tests/test_input_reading.py
@@ -34,7 +34,17 @@ class Test_parsing_input_file(unittest.TestCase):
         self.logger.addHandler(logging.NullHandler())
 
         # Path to example input file provided with the programme
-        self.input_file_path = self.input_dir / "test_input_file.txt"
+        self.input_reading_path = self.input_dir / "test_input_file.txt"
+
+        # Retrieve 'retries' argument from test inputs
+        self.input_file_path = self.input_dir / "test_inputs.txt"
+
+        with open(self.input_file_path) as file:
+            input_list = file.read().splitlines()
+
+        for line in input_list:
+            if line.startswith("retries"):
+                self.retries = line[-1:]
 
     # Define tests
 
@@ -42,10 +52,12 @@ class Test_parsing_input_file(unittest.TestCase):
     def test_example_input_file_exists(self):
         """Tests that test input file, supplied with programme is present."""
 
-        self.assertTrue(self.input_file_path.is_file())
+        self.assertTrue(self.input_reading_path.is_file())
 
     @pytest.mark.run(order=2)
     def test_input_file(self):
         """Tests script can open and read supplied input file."""
 
-        Extract_genomes_NCBI.parse_input_file(self.input_file_path, self.logger)
+        Extract_genomes_NCBI.parse_input_file(
+            self.input_reading_path, self.logger, self.retries
+        )
