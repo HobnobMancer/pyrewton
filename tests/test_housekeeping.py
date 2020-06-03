807c7c5999ee66cb7a10d56135b48d424954f7b2
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-06-03 10:57:23 +0100

    add df writing out test

--- tests/test_housekeeping.py
+++ tests/test_housekeeping.py
@@ -7,6 +7,7 @@ import unittest
 from argparse import Namespace
 from pathlib import Path
 
+import pandas as pd
 import pytest
 
 from Bio import Entrez
@@ -33,6 +34,7 @@ class Test_housekeeping_functions(unittest.TestCase):
         self.output_dir = (
             self.test_dir / "test_targets" / "test_ext_gnm_ncbi" / "EgN_target_dir"
         )
+        self.df_output = self.output_dir / "test_df.csv"
 
         # Null logger instance
         self.logger = logging.getLogger("Test_name_and_ID_Retrieval logger")
@@ -43,6 +45,10 @@ class Test_housekeeping_functions(unittest.TestCase):
 
         # Define Namespace and disable genbank download
         self.argsdict = {"args": Namespace(verbose=False, log=None)}
+
+        # Create dataframe (df)
+        self.df_data = [["A", "B", "C"]]
+        self.df = pd.DataFrame(self.df_data, columns=["C1", "C2", "C3"])
 
     # Define function to test
 
@@ -61,4 +67,11 @@ class Test_housekeeping_functions(unittest.TestCase):
         """Tests function for creating output dir"""
         Extract_genomes_NCBI.make_output_directory(
             self.output_dir, self.logger, True, True
+        )
+
+    @pytest.mark.run(order=4)
+    def test_writing_df(self):
+        """Tests function for writing out created dataframe"""
+        Extract_genomes_NCBI.write_out_dataframe(
+            self.df, self.logger, self.df_output, True, False
         )
