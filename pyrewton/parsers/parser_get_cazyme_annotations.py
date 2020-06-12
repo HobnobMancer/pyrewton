6ae43adf90e80d4d07c79df2c89a46fd6e03903c
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2020-06-12 14:27:55 +0100

    Create parser_get_cazyme_annotations.py
    Move build_parser() function from get_cazyme_annotations.py into separate parser module

---
+++ pyrewton/parsers/parser_get_cazyme_annotations.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:
+# Emma E. M. Hobbs
+
+# Contact
+# eemh1@st-andrews.ac.uk
+
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+
+# The MIT License
+"""Build parser for 'get_cazyme_annotations.py'"""
+
+import argparse
+import sys
+
+from pathlib import Path
+from typing import List, Optional
+
+def build_parser(argv: Optional[List] = None):
+    """Return ArgumentParser parser for script."""
+    # Create parser object
+    parser = argparse.ArgumentParser(
+        prog="cazy_genbank_summary.py",
+        description="Generate summary of CAZy annotation in GenBank files",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    # Add arguments to parser
+    # Add option to specific input directory for dataframe
+    parser.add_argument(
+        "-d",
+        "--df_input",
+        type=Path,
+        metavar="input datafram name",
+        default=sys.stdin,
+        help="input dataframe path",
+    )
+    # Add option to force file over writting
+    parser.add_argument(
+        "-f",
+        "--force",
+        dest="force",
+        action="store_true",
+        default=False,
+        help="Force file over writting",
+    )
+    # Add option to specific input directory for GenBank files
+    parser.add_argument(
+        "-g",
+        "--genbank",
+        type=Path,
+        metavar="GenBank file directory",
+        default=sys.stdin,
+        help="GenBank file path directory",
+    )
+    # Add option to specific directory for log to be written out to
+    parser.add_argument(
+        "-l",
+        "--log",
+        type=Path,
+        metavar="log file name",
+        default=None,
+        help="Defines log file name and/or path",
+    )
+    # Add option to prevent over writing of existing files
+    # and cause addition of files to output directory
+    parser.add_argument(
+        "-n",
+        "--nodelete",
+        dest="nodelete",
+        action="store_true",
+        default=False,
+        help="enable/disable deletion of exisiting files",
+    )
+    # Add option to specific directory for output to be written to
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=Path,
+        metavar="output file name",
+        default=sys.stdout,
+        help="output filename",
+    )
+
+    if argv is None:
+        # parse command-line
+        return parser
+    else:
+        # return namespace
+        return parser.parse_args(argv)
