f4bc3b6bf2fee8bb58a998ddc1cda97b676a2063
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2020-06-17 10:11:52 +0100

    Add author dets to pyrewton _init_

--- pyrewton/__init__.py
+++ pyrewton/__init__.py
@@ -1,2 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:
+# Emma E. M. Hobbs
+# 
+# Contact
+# eemh1@st-andrews.ac.uk
+# 
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+# 
+# The MIT License
 """Python programme for identifying engineering candidates
 for advanced biocatalysis in biofuel production."""
