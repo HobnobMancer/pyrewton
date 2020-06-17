d87e779e7fced15ee1ef3476e2e870c0fd308289
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2020-06-17 10:17:33 +0100

    add author dets to get_ncbi_genomes init

--- pyrewton/genbank/get_ncbi_genomes/__init__.py
+++ pyrewton/genbank/get_ncbi_genomes/__init__.py
@@ -1,2 +1,19 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Author:
+# Emma E. M. Hobbs
+# 
+# Contact
+# eemh1@st-andrews.ac.uk
+# 
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+# 
+# The MIT License
 """Submodule with code to pull down genomic assemblies from NCBI database."""
