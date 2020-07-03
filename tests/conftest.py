3b3c04fc72013e3b36cbff79a47f8a51d532202a
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2020-07-03 16:42:13 +0100

    Create conftest.py
    Create conftest.py to store global pytest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:
+# Emma E. M. Hobbs
+
+# Contact
+# eemh1@st-andrews.ac.uk
+
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+
+# The MIT License
+"""Configuration file for pytest files.
+
+Contains fixtures used by multiple test files.
+"""
+
+import json
+import logging
+
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def dumpy_email():
+    email = "dumpy.email@my.domain"
+    return email
+
+
+@pytest.fixture
+def null_logger():
+    logger = logging.getLogger("Test_ac_number_retrieval logger")
+    logger.addHandler(logging.NullHandler())
+    return logger
+
+
+@pytest.fixture
+def gt_ncbi_gnms_input_dir():
+    test_dir = Path("tests")
+    input_dir = test_dir / "test_inputs" / "gt_ncbi_gnms_test_inputs"
+    return input_dir
+
+
+@pytest.fixture
+def gt_ncbi_gnms_test_inputs(gt_ncbi_gnms_input_dir):
+    with (gt_ncbi_gnms_input_dir / "gt_ncbi_gnms_test_inputs.json").open("r") as ifh:
+        test_inputs = json.load(ifh)
+        inputs = []
+        inputs.append(test_inputs["retries"])
+        inputs.append(test_inputs["taxonomy_id"])
+        inputs.append(test_inputs["genus_species_name"])
+        inputs.append(test_inputs["line_number"])
+        inputs.append(test_inputs["df_row_genus"])
+        inputs.append(test_inputs["df_row_species"])
+    return inputs
+
+
+@pytest.fixture
+def gt_ncbi_gnms_targets():
+    test_dir = Path("tests")
+    target_dir = test_dir / "test_targets" / "gt_ncbi_gnms_test_targets"
+    with (target_dir / "gt_ncbi_gnms_test_targets.json").open("r") as ifht:
+        test_targets = json.load(ifht)
+        target_sci_name = test_targets["target_genus_species_name"]
+        target_tax_id = test_targets["target_taxonomy_id"]
+    return target_sci_name, target_tax_id, target_dir
