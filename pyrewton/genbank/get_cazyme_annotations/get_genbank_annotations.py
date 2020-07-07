0b644604c1aaaf9d53b348ecfb84694a0feb804b
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2020-07-07 14:26:37 +0100

    rename and update get_cazymes_annotations.py
    Rename get_cazyme_annotations.py to get_genbank_annotations.py.
    Remove get_genbank_file() func from file_io and re-introduce to get_genbank_annotations.py.
    Modify script to end after retrieving all protein annotations from the GenBank files.

--- pyrewton/genbank/get_cazyme_annotations/get_genbank_annotations.py
+++ pyrewton/genbank/get_cazyme_annotations/get_genbank_annotations.py
@@ -16,7 +16,7 @@
 # UK
 #
 # The MIT License
-"""Create summary of annotated CAZy classes in GenBank files.
+"""Retrieve all protein annotations from GenBank files.
 
 :cmd_args df_input: path, path to input dataframe
 :cmd_args force: bool, force overwriting files in output directory
@@ -31,7 +31,7 @@
 :func build_df_foundation: Compile row data for dataframe
 :func get_genbank_protein_data: Retrieve protein name and IDs
 
-Generate summary dataframe and of annotated cazymess in all GenBank
+Generate summary dataframe and of all annotated proteins in all GenBank
 files directly linked to a given species.
 """
 
@@ -51,7 +51,7 @@ from pandas.errors import EmptyDataError
 from tqdm import tqdm
 from urllib.error import HTTPError
 
-from pyrewton.file_io import make_output_directory, input_dir_get_cazyme_annotations
+from pyrewton import file_io
 from pyrewton.loggers import build_logger
 from pyrewton.parsers.parser_get_cazyme_annotations import build_parser
 
@@ -92,18 +92,14 @@ def main(argv: Optional[List[str]] = None, logger: Optional[logging.Logger] = No
     input_df = pd.read_csv(args.df_input, header=0, index_col=0)
 
     # Build dataframe
-    cazy_summary_df = create_dataframe(input_df, args, logger)
-
-    # Create summary charts of CAZy annotation distribution
+    protein_annotation_df = create_dataframe(input_df, args, logger)
 
     # Write out dataframe
+    file_io.write_out_dataframe(protein_annotation_df, logger, args.output, args.force, args.nodelete)
 
 
 def create_dataframe(input_df, args, logger):
-    """Build datafame.
-
-    -- Dev note: add explanation for for loops and not apply:
-    httpS://ys-l.github.io/posts/2015/08/28/how-not-to-use-pandas-apply
+    """Build datafame containing all protein annotations in GenBank files.
 
     Iterate over input dataframe row wise. This allows for converting human
     readable accession number list from a string to a Python list.
@@ -117,11 +113,6 @@ def create_dataframe(input_df, args, logger):
     used simultaneously to populate a pandas dataframe without risk of
     data missalignment or over writing.
 
-    Then iterate over protein ID column to retrieve CAZy data if available,
-    allowing for faster retrieval of data from UniProt.
-
-    Any failed to retrieve data will be returned as pandas null value 'NA'.
-
     :param input_df: pandas dataframe
     :param args: parser arguments
     :param logger: logger object
@@ -129,7 +120,7 @@ def create_dataframe(input_df, args, logger):
     Return dataframe.
     """
     # Create empty dataframe to add data to
-    cazy_summary_df = pd.DataFrame(
+    protein_annotation_df = pd.DataFrame(
         columns=[
             "Genus",
             "Species",
@@ -147,55 +138,18 @@ def create_dataframe(input_df, args, logger):
     df_index = 0
     for df_index in range(len(input_df["Genus"])):
         cazy_summary_df = cazy_summary_df.append(
-            get_df_foundation_data(input_df.iloc[df_index], args, logger),
+            get_genbank_annotations(input_df.iloc[df_index], args, logger),
             ignore_index=True,
         )
         df_index += 1
 
     # these are debugging purposes and will not be included in final version
-    print("=====Foundation dataframe======\n", cazy_summary_df, "\n")
-    cazy_summary_df.to_csv("foundation_dataframe.csv")
-
-    # Build empty dataframe to store data retrieved from UniProtKB
-    # GO = Gene Ontology (GO) project
-    all_uniprotkb_data_df = pd.DataFrame(
-        columns=[
-            "UniProt entry ID",
-            "UniProt entry name",
-            "UniProt assigned protein names",
-            "Length (Aa)",
-            "Mass (Da)",
-            "Domains",
-            "Domain count",
-            "UniProt linked protien families",
-            "GO IDs",
-            "GO molecular function",
-            "GO biologocial process",
-        ]
-    )
-
-    # parse current cazy_summary_df, retreiving UniProtKB data for each protein
-    df_index = 0
-    for df_index in tqdm(
-        range(len(cazy_summary_df["Genus"])), desc="Retrieving UniProt data"
-    ):
-        all_uniprotkb_data_df = all_uniprotkb_data_df.append(
-            get_uniprotkb_data(cazy_summary_df.iloc[df_index], logger),
-            ignore_index=True,
-        )
-        df_index += 1
-
-    print("=====\nUniProt dataframe:\n", all_uniprotkb_data_df)
-
-    # join UniProt dataframe to foundation dataframe
-    cazy_summary_df = pd.concat([cazy_summary_df, all_uniprotkb_data_df], axis=1)
-
-    print("=====\nFoundatoin + UniProt dataframe:\n", all_uniprotkb_data_df)
+    print("=====Foundation dataframe======\n", protein_annotation_df, "\n")
 
     return cazy_summary_df
 
 
-def get_df_foundation_data(df_row, args, logger):
+def get_genbank_annotations(df_row, args, logger):
     """Prepare row data to create dataframe.
 
     Coordinate retrieval of protein data from GenBank files for every accession
@@ -243,7 +197,7 @@ def get_df_foundation_data(df_row, args, logger):
     # all protein data in that GenBank file, stored as a tuple with each
     # list in the tuple containing data for a unique protein
     for accession in accession_list:
-        protein_data = get_genbank_protein_data(accession, args, logger)  # tuple
+        protein_data = get_annotations(accession, args, logger)  # tuple
 
         # check if any data retrieved
         if len(protein_data) == 0:
@@ -297,7 +251,7 @@ def get_df_foundation_data(df_row, args, logger):
     return protein_data_df
 
 
-def get_genbank_protein_data(accession_number, args, logger):
+def get_annotations(accession_number, args, logger):
     """Retrieve protein ID, locus tag and function from GenBank file.
 
     From each record the protein ID, locus tag, location and annotated
@@ -325,10 +279,7 @@ def get_genbank_protein_data(accession_number, args, logger):
         return ["NA", "NA", "NA", "NA", "NA"]
 
     # retrieve GenBank file for accession number
-    gb_file = input_dir_get_cazyme_annotations.get_genbank_file(
-        accession_number, args, logger
-    )  # list with GenBank file with index [0]
-
+    gb_file = get_genbank_file(accession_number, args, logger)  # list with GenBank file with index [0]
     # If retrieving of GenBank file failed, return 'NA' for all protein data
     # for accession number
     if gb_file is None:
@@ -394,6 +345,62 @@ def get_genbank_protein_data(accession_number, args, logger):
     return all_protein_data
 
 
+def get_genbank_file(accession, args, logger):
+    """Retrieve GenBank file for accession number in local dir.
+
+    :param accession: str, accession number of GenBank file
+    :param args: parser arguments
+    :param logger: logger object
+
+    Return list of length 1, containing path to GenBank file.
+    """
+    # replace '.' with '_' to match format in GenBank file name
+    file_stem = accession.replace(".", "_")
+
+    # create empty list to store file entries, to allow checking if multiple files were retrieved
+    gb_file = []
+
+    # retrieve all files from directory
+    files_in_entries = (
+        entry for entry in Path(args.genbank).iterdir() if entry.is_file()
+    )
+    for item in files_in_entries:
+        # search for accession number's GenBank file
+        if item.name.startswith(f"{file_stem}") and item.name.endswith(".gbff.gz"):
+            gb_file.append(item)
+
+    # check file was retrieved, not multiple or none
+    if len(gb_file) == 0:
+        logger.warning(
+            (
+                f"Retrieved 0 files for {accession}.\n"
+                "Returning null ('NA') value for all protein data"
+            )
+        )
+        return None
+
+    elif len(gb_file) > 1:
+        logger.warning(
+            (
+                f"Retrieved multiple files for {accession}.\n"
+                "Returning null ('NA') value for all protein data"
+            )
+        )
+        return None
+
+    # check if files is empty
+    if gb_file[0].stat().st_size == 0:
+        logger.warning(
+            (
+                f"GenBank file retrieved for {accession} is empty.\n"
+                "Returning null ('NA' value for all protein data"
+            )
+        )
+        return None
+
+    return gb_file
+
+
 def get_record_feature(feature, qualifier, logger):
     """Retrieve data from GenBank record feature.
 
@@ -425,122 +432,6 @@ def get_record_feature(feature, qualifier, logger):
                 f"Failed to retrieve feature {qualifier}, returning 'NA'", exc_info=1
             )
             return "NA"
-
-
-def get_uniprotkb_data(df_row, logger):
-    """Coordinate retrieval of protein data from UniProtKB.
-
-    Reminder of structure of df_row (row from cazy_summary_df) when
-    retrieving specific data
-    df_row[0] = genus
-    df_row[1] = species
-    df_row[5] = protein unique locus tag
-
-    :param df_row: pandas series, row from cazy_summary_df dataframe
-    :param logger: logger object
-
-    Return dataframe.
-    """
-    # establish data to be retrieved from UniProt
-    columnlist = (
-        "id,entry name,protein names,length,mass,domains,domain,"
-        "families,"
-        "go-id,go(molecular function),go(biological process)"
-    )
-
-    # This dictionary will be used to populate "blank"/"empty" databases when
-    # an error is thrown. Iterables are used as values to avoid problems with
-    # "ValueError: If using all scalar values, you must pass an index"
-    blank_data = {
-        "UniProtKB Entry ID": ["NA"],
-        "UniProtKB Entry Name": ["NA"],
-        "UniProtKB Protein Names": ["NA"],
-        "EC number": ["NA"],
-        "Length (Aa)": ["NA"],
-        "Mass (Da)": ["NA"],
-        "Domains": ["NA"],
-        "Domain count": ["NA"],
-        "UniProtKB Linked Protein Families": ["NA"],
-        "Gene ontology IDs": ["NA"],
-        "Gene ontology (molecular function)": ["NA"],
-        "Gene ontology (biological process)": ["NA"],
-    }
-
-    try:
-        # open connection to UniProt(), search and convert result into pandas df
-        logger.info(df_row)
-        query = f'{df_row[5]} AND organism:"{df_row[0]} {df_row[1]}"'
-        search_result = UniProt().search(
-            query, columns=columnlist,
-        )  # returns empty string for no result
-        logger.info(search_result)
-        search_result_df = pd.read_table(io.StringIO(search_result))
-
-    except HTTPError:
-        logger.warning(
-            (
-                f"Network error occured when searching UniProt for locus tag:{df_row[5]}.\n"
-                "Returning null value 'NA' for all UniProt data"
-            )
-        )
-        return pd.DataFrame(blank_data)
-
-    except EmptyDataError:
-        # No UniProt entries found for locus tag, return null data for
-        logger.warning(
-            (
-                f"No data returned from UniProt for locus tag:{df_row[5]}.\n"
-                "Returning null value 'NA' for all UniProt data"
-            )
-        )
-        return pd.DataFrame(blank_data)
-
-    # check if multiple entries were returned
-    if len(search_result_df) > 1:
-        logger.warning(
-            (
-                f"Multiple hits returned from UniProt for locus tag:{df_row[5]}.\n"
-                "Returning null value 'NA' for all UniProt data"
-            )
-        )
-        return pd.DataFrame(blank_data)
-
-    # rename columns to match to indicate UniProtKB source of data
-    search_result_df.rename(
-        columns={
-            "Entry": "UniProtKB Entry ID",
-            "Entry name": "UniProtKB Entry Name",
-            "Protein names": "UniProtKB Protein Names",
-            "Length": "Length (Aa)",
-            "Mass": "Mass (Da)",
-            "Protein families": "UniProtKB Linked Protein Families",
-        }
-    )
-
-    # Retrieve EC number from 'UniProtKB Protein Names'
-    # or return 'NA' if not included
-    EC_search = re.findall(
-        r"\(EC [\d-]\d*\.[\d-]\d*\.[\d-]\d*\.[\d-]\d*\)", search_result_df[2]
-    )
-    if EC_search is None:
-        EC_number = "NA"
-    else:
-        # compiall EC together incase multiple are given
-        # and remove EC numbers from protein name
-        EC_number = ""
-        for EC in EC_search:
-            search_result_df[2] = search_result_df[2].replace(EC, "")
-            EC = EC.replace("(", "")
-            EC = EC.replace(")", "")
-            EC_number += EC
-            # Do I want to remove the EC number becuase if multiple are given,
-            # maybe helpful to see in the protein name section which EC number
-            # corresponds to which function
-    # Add EC number to dataframe0
-    search_result_df.insert(3, "EC number", EC_number)
-
-    return search_result_df
-
 
 if __name__ == "__main__":
     main()
