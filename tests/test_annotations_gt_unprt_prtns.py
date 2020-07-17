a14b88db18084d24001fb96bbfecea4e8e8fa986
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-07-17 19:49:49 +0100

    add local fixtures for testing pyrewton annotations module

--- tests/test_annotations_gt_unprt_prtns.py
+++ tests/test_annotations_gt_unprt_prtns.py
@@ -22,27 +22,92 @@
 These tests are inteded to be run from the root repository using:
 pytest -v
 """
+
 import pytest
+
+from argparse import Namespace
+
+from bioservices import UniProt
 
 from pyrewton.annotations import get_uniprot_proteins
 
 
+@pytest.fixture
+def genbank_df_path(test_dir):
+    genbank_df = (
+        test_dir / "test_inputs" / "gt_gnbnk_anntns_test_inputs" / "test_input_df.csv"
+    )
+    return genbank_df
+
+
+@pytest.fixture
+def genbank_files_dir(test_dir):
+    genbank_dir = (
+        test_dir / "test_inputs" / "gt_gnbnk_anntns_test_inputs" / "test_genbank_files"
+    )
+    return genbank_dir
+
+
+@pytest.fixture
+def gnbnk_anno_args(genbank_files_dir):
+    argsdict = {"args": Namespace(genbank=genbank_files_dir)}
+    return argsdict
+
+
+@pytest.fixture
+def genbank_df(genbank_df_path):
+    input_df = pd.read_csv(genbank_df_path, header=0, index_col=0)
+    return input_df
+
+
+@pytest.fixture
+def formated_uniprot_results(test_dir):
+    df_dir = (
+        test_dir
+        / "test_inputs"
+        / "gt_unprt_prtns_test_inputs"
+        / "mocked_formated_uniprot_results.csv"
+    )
+    df = pd.read_csv(df_dir, header=0, index_col=0)
+    return df
+
+
+@pytest.fixture
+def unformated_uniprot_results(test_dir):
+    uniprot_results = (
+        test_dir
+        / "test_inputs"
+        / "gt_unprt_prtns_test_inputs"
+        / "mocked_unformated_uniprot_results.txt"
+    )
+    with open(uniprot_results, "r") as results:
+        data = results
+    return data
+
+
+@pytest.fixture
+def uniprot_query_source(formated_uniprot_results):
+    """Return single row, a pandas series."""
+    return formated_uniprot_results.head(1)
+
+
 @pytest.mark.run(order=15)
-def uniprot_df_creation(genbank_df, null_logger, formated_uniprot_results, monkeypatch):
+def test_uniprot_df_creation(
+    genbank_df, null_logger, unformated_uniprot_results, monkeypatch
+):
     """Test creation and processing of dataframe of UniProt results."""
 
-    def mock_uniprot_call(*args, **kwargs):
+    def mock_bioservices_uniprot(*args, **kwargs):
         """Mocks call to UniProtKB database."""
-        result = formated_uniprot_results
-        return result
+        return unformated_uniprot_results
 
-    monkeypatch.setattr(get_uniprot_proteins, "get_uniprotkb_data", mock_uniprot_call)
+    monkeypatch.setattr(UniProt, "search", mock_bioservices_uniprot)
 
     get_uniprot_proteins.build_uniprot_df(genbank_df, null_logger)
 
 
 @pytest.mark.run(order=16)
-def format_uniprot_results(
+def test_format_uniprot_results(
     unformated_uniprot_results, uniprot_query_source, null_logger
 ):
     """Test formating of UniProt search results."""
