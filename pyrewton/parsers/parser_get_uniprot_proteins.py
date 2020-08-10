7d7e28f72bfdf37ac78ddfa178532985f2446327
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-08-10 20:29:43 +0100

    update parser for get_uniprot_proteins
    add additional arguments to parser (-fa) and update descriptions of exisiting for role matches role in get_uniprot_proteins.py

--- pyrewton/parsers/parser_get_uniprot_proteins.py
+++ pyrewton/parsers/parser_get_uniprot_proteins.py
@@ -29,20 +29,29 @@ def build_parser(argv: Optional[List] = None):
     """Return ArgumentParser parser for script."""
     # Create parser object
     parser = argparse.ArgumentParser(
-        prog="cazy_genbank_summary.py",
-        description="Generate summary of CAZy annotation in GenBank files",
+        prog="get_uniprot_proteins.py",
+        description="Retrieve protein data from UniProtKB",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
     # Add arguments to parser
-    # Add option to specific input directory for dataframe
+    # Add option to specific input config file
     parser.add_argument(
-        "-d",
-        "--df_input",
+        "-i",
+        "--input",
         type=Path,
-        metavar="input dataframe name",
+        metavar="configuration file",
         default=sys.stdin,
-        help="input dataframe path",
+        help="configuration file path",
+    )
+    # Add option to enable/disable fasta file writing
+    parser.add_argument(
+        "-fa",
+        "--fasta",
+        desc="fasta"
+        action="store_true"
+        default=False,
+        help="Enable fasta file writing"
     )
     # Add option to force file over writting
     parser.add_argument(
@@ -77,9 +86,9 @@ def build_parser(argv: Optional[List] = None):
         "-o",
         "--output",
         type=Path,
-        metavar="output file name",
+        metavar="output directory path",
         default=sys.stdout,
-        help="output filename",
+        help="output directory path",
     )
     # Add option to specify verbose logging
     parser.add_argument(
