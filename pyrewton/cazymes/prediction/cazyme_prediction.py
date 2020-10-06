99a63714659979aa78a0fe4b05cfca507cce9d8f
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-10-06 14:11:32 +0100

    create script to coordinate prediction of CAZymes
    Add function to retrieve list of paths to all fasta file in input directory.

---
+++ pyrewton/cazymes/prediction/cazyme_prediction.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:
+# Emma E. M. Hobbs
+#
+# Contact
+# eemh1@st-andrews.ac.uk
+#
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+#
+# The MIT License
+"""Predict CAZymes from protein sequences and evaluate prediction accuracy.
+
+:cmd_args   :
+
+:func    :
+
+Creates dataframes of CAZyme predictions and report
+summarising statistical analsis of prediction accuracy.
+"""
+
+import sys
+
+from pathlib import Path
+
+
+def main():
+    """Set up utilities and coordinate prediction of CAZymes."""
+    # build parser
+
+    # build logger
+
+    # retrieve paths to fasta files
+    fasta_file_paths = get_fasta_paths(args, logger)
+
+    # for each FASTA file invoke dbCAN, CUPP and eCAMI
+    for file_path in fasta_file_paths:
+        # ...
+
+
+def get_fasta_paths(args, logger):
+    """Retrieve paths to call FASTA files in input dir.
+    
+    :param args: parser object
+    :param logger: logger object
+
+    Returns list of paths to fasta files.
+    """
+    # create empty list to store the file entries, to allow checking if no files returned
+    fasta_file_paths = []
+
+    # retrieve all files from input directory
+    files_in_entries = (entry for entry in Path(args.input).iterdir if entry.is_file())
+
+    # retrieve paths to fasta files
+    for item in files_in_entries:
+        # search for fasta file extension
+        if item.name.endswith(".fasta") or item.name.endswith(".fa"):
+            fasta_file_paths.append(item)
+
+    # check files were retrieved from input directory
+    if len(fasta_file_paths) == 0:
+        logger.warning(
+            (
+                "No FASTA files retrieved from input directory.\n"
+                "Check the path to the input directory is correct.\n"
+                "Terminanting program."
+            )
+        )
+        sys.exit(1)
+
+    return fasta_file_paths
+
+if __name__ == "__main__":
+    main()
