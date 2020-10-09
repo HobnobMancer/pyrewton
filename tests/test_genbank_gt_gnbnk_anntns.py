e8d87a288ad5df96f9b755946c7e58e2c8696825
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-10-09 16:08:12 +0100

    update args parser in testing of main()

--- tests/test_genbank_gt_gnbnk_anntns.py
+++ tests/test_genbank_gt_gnbnk_anntns.py
@@ -140,12 +140,16 @@ def test_main(null_logger, output_dir, coordination_args, test_input_df, monkeyp
             output=output_dir,
             force=False,
             genbank=gb_file_dir,
-            df_input=test_input_df_path,
+            input_df=test_input_df_path,
+            output_df=test_input_df_path,
         )
         return parser
 
     def mock_build_logger(*args, **kwargs):
         return null_logger
+
+    def mock_create_out_dir(*args, **kwargs):
+        return
 
     def mock_df_reading(*args, **kwargs):
         return test_input_df_path
@@ -157,6 +161,9 @@ def test_main(null_logger, output_dir, coordination_args, test_input_df, monkeyp
     def mock_write_out_dataframe(*args, **kwargs):
         return
 
+    def mock_write_fasta(*args, **kwargs):
+        return
+
     monkeypatch.setattr(get_genbank_annotations, "build_parser", mock_built_parser)
     monkeypatch.setattr(ArgumentParser, "parse_args", mock_parser)
     monkeypatch.setattr(get_genbank_annotations, "build_logger", mock_build_logger)
@@ -166,6 +173,10 @@ def test_main(null_logger, output_dir, coordination_args, test_input_df, monkeyp
     )
     monkeypatch.setattr(
         get_genbank_annotations, "write_out_dataframe", mock_write_out_dataframe
+    )
+    monkeypatch.setattr(get_genbank_annotations, "write_fasta", mock_write_fasta)
+    monkeypatch.setattr(
+        get_genbank_annotations, "make_output_directory", mock_create_out_dir
     )
 
     get_genbank_annotations.main()
