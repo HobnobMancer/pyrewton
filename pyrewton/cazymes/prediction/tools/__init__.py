dfac1759fce5ba435cde60b9862b28caa756683b
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-10-14 16:21:12 +0100

    write function to invoke dbcan, ecami, cupp
    compiles args to invoke each prediction tool, checks if successful, and writes out terminal output from each prediction tool to a individual log file per tool. Output from all tools are written to the same output directory

--- pyrewton/cazymes/prediction/tools/__init__.py
+++ pyrewton/cazymes/prediction/tools/__init__.py
@@ -18,18 +18,91 @@
 # The MIT License
 """Module invoking CAZyme prediction tools: dbCAN, CUPP and eCAMI"""
 
+import subprocess
+
 
 def invoke_prediction_tools(query, args, logger):
     """Pass paramaters to CAZyme prediction tools and invoke tool run."""
     fasta_path = query.fasta
     outdir = query.prediction_dir
 
-    # args required by run_dbCAN
-    # <fasta_path> protein --output_dir <outdir_path>
+    # variables to store successful/failed process return codes
+    dbcan_returncode = 0
+    cupp_returncode = 0
+    ecami_returncode = 0
 
-    # args required by ecami for prediction.py
-    # -input <fasta_path> -kmer_db CAZyme -output <path to output file, including .text ext>
+    # create list of args to invoke run_dbCAN
+    dbcan_args = [
+        "tools/dbcan/run_dbcan",
+        str(fasta_path),
+        "protein",
+        "--output_dir",
+        str(outdir),
+    ]
 
-    # args required by cupp prediction.py
-    # -query <fasta_path> -output_path <path to output fasta file including .fasta ext>
+    # create log of dbCAN run
+    with open(f"{outdir}/dbcan.log") as fh:
+        process = subprocess.run(dbcan_args, stdout=fh, text=True)
 
+    # check if successul
+    if process.returncode != 0:  # return code is 0 for successful run
+        logger.warning(
+            (
+                f"dbCAN ran into error for {outdir[-1]}\n."
+                "dbCAN error:\n"
+                f"{process.stderr}"
+            )
+        )
+        dbcan_returncode = 1
+
+    # create list of args to invoke cupp
+    cupp_args = [
+        "tools/cupp/prediction.py",
+        "-query",
+        str(fasta_path),
+        "-output_path",
+        f"{outdir}/cupp_output.fasta",
+    ]
+
+    # create log of CUPP run
+    with open(f"{outdir}/cupp.log") as fh:
+        process = subprocess.run(cupp_args, stdout=fh, text=True)
+
+    # check if successful
+    if process.returncode != 0:
+        logger.warning(
+            (
+                f"CUPP ran into error for {outdir[-1]}\n."
+                "CUPP error:\n"
+                f"{process.stderr}"
+            )
+        )
+        cupp_returncode = 1
+
+    # create list of args for ecami
+    ecami_args = [
+        "tools/ecami/prediction.py",
+        "-input",
+        str(fasta_path),
+        "-kmer_db",
+        "CAZyme",
+        "-output",
+        f"{outdir}/ecami_output.txt",
+    ]
+
+    # create log of eCAMI run
+    with open(f"{outdir}/ecami.log", "w") as fh:
+        process = subprocess.run(ecami_args, stdout=fh, text=True)
+
+    # check if successful
+    if process.returncode != 0:
+        logger.warning(
+            (
+                f"eCAMI ran into error for {outdir[-1]}\n."
+                "eCAMI error:\n"
+                f"{process.stderr}"
+            )
+        )
+        ecami_returncode = 1
+
+    return dbcan_returncode, cupp_returncode, ecami_returncode
