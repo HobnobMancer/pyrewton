04031439a92e4528ee0d53489b8f4c72bf6a7a9c
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-12-02 23:20:33 +0000

    write functions to retrieve CAZy df and dict of genbank synonyms

--- pyrewton/cazymes/prediction/parse/__init__.py
+++ pyrewton/cazymes/prediction/parse/__init__.py
@@ -19,16 +19,25 @@
 """Module to parse the output from dbCAN, CUPP and eCAMI. Create standarised output."""
 
 
+import json
+import re
+
 import pandas as pd
 
 
 def get_cazy_accessions(args, logger):
-    """Retrieve GenBank and UniProt accession numbers of proteins in CAZy dataframe.
+    """Retrieve dataframe of CAZymes catalogued in CAZy and dictionary of GenBank synonyms.
+
+    Returns the CAZy family, GenBank and UniProt columns from the CAZy dataframe. GenBank synonyms
+    are the GenBank accessions listed under a CAZyme in addition to the first GenBank accession
+    listed for the CAZyme in CAZy and which is always hyperlinked to GenBank. The GenBank synonyms
+    are frequently annotated as 'identical proteins' to the CAZyme first GenBank accession in
+    GenBank.
 
     :param args: cmd args parser
     :param logger: logger object
 
-    Return two lists of accession numbers.
+    Return Pandas dataframe.
     """
     # open the cazy df
     try:
@@ -42,28 +51,46 @@ def get_cazy_accessions(args, logger):
         )
         return None, None
 
-    # create empty list to store accessions in
-    genbank_accessions = []
-    uniprot_accessions = []
+    # Parse the CAZy dataframe to retrieve only the CAZy family, GenBank and UniProt columns
+    cazy_df = cazy_df.drop(["Protein_name", "EC#", "Source_organism", "PDB/3D"], axis=1)
 
-    # get genbank accession numbers
-    index = 0
-    for index in range(len(cazy_df["Protein_name"])):
-        cell_content = cazy_df[index, 4]
-        cell_content = cell_content.split(",\n")
-        i = 0
-        for i in range(len(cell_content)):
-            accession = cell_content[i][:(cell_content[1].find(" "))]
-            genbank_accessions.append(accession)
+    genbank_synonyms = get_genbank_synonyms(args, logger)
 
-    # get uniprot accession numbers
-    index = 0
-    for index in range(len(cazy_df["Protein_name"])):
-        cell_content = cazy_df[index, 5]
-        cell_content = cell_content.split(",\n")
-        i = 0
-        for i in range(len(cell_content)):
-            accession = cell_content[i][:(cell_content[1].find(" "))]
-            uniprot_accessions.append(accession)
+    return cazy_df, genbank_synonyms
 
-    return genbank_accessions, uniprot_accessions
+
+def get_genbank_synonyms(args, logger):
+    """Retrieve dictionary of GenBank synonyms from JSON file.
+
+    :param args: cmd args parser
+    :param logger: logger object
+
+    Return dictionary of GenBank synonyms.
+    """
+    # Compile path to GenBank synonym dictionary
+    cazy_path = str(args.cazy)
+    try:
+        cazy_filename = re.match(r"cazy_.+?\.csv", cazy_path, re.IGNORECASE).group()
+        dict_filename = cazy_filename.replace("cazy_", "cazy_genbank_synonyms_")
+        dict_filename = dict_filename.replace(".csv", ".json")
+        dict_path = cazy_path.replace(cazy_filename, dict_filename)
+    except AttributeError:
+        logger.warning(
+            "Did not find a JSON file of GenBank synonyms from CAZy.\n"
+            "Continuing with evaluating CAZyme prediction tools without GenBank synonyms."
+        )
+        dict_path = None
+        genbank_synonyms = {}
+
+    # Open JSON file
+    if dict_path is not None:
+        try:
+            with open(dict_path, "r") as fh:
+                genbank_synonyms = json.load(fh)
+        except FileNotFoundError:
+            logger.error(
+                "Could not open the JSON file of GenBank synonyms from CAZy.\n"
+                "Continuing with evaluating CAZyme prediction tools without GenBank synonyms."
+            )
+
+    return genbank_synonyms
