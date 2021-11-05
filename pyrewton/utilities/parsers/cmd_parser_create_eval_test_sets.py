2d87b298c8f15dd1422a35d729468ae5330b8045
HobnobMancer <hobnobmancer@Paddington.localdomain>
2021-11-05 10:35:13 +0000

    create single script for both 'create_test_sets_*.py' scrupts

--- pyrewton/utilities/parsers/cmd_parser_create_eval_test_sets.py
+++ pyrewton/utilities/parsers/cmd_parser_create_eval_test_sets.py
@@ -16,7 +16,8 @@
 # UK
 
 # The MIT License
-"""Build parser for 'cmd_parser_get_evaluation_dataset_from_dict.py'"""
+"""Build parser for 'create_evaluation_tests_set_from_*.py'"""
+
 
 import argparse
 
@@ -24,11 +25,11 @@ from pathlib import Path
 from typing import List, Optional
 
 
-def build_parser(argv: Optional[List] = None):
+def build_parser_dict(argv: Optional[List] = None):
     """Return ArgumentParser parser for script."""
     # Create parser object
     parser = argparse.ArgumentParser(
-        prog="cmd_parser_get_evaluation_dataset_from_dict.py",
+        prog="create_test_set_from_dict.py",
         description="Build test sets for evaluating CAZyme prediction tools",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
@@ -51,6 +52,106 @@ def build_parser(argv: Optional[List] = None):
         "cazy",
         type=Path,
         help="Path to JSON file containing dict of GenBank accesions and CAZy familes from CAZy",
+    )
+
+    parser.add_argument(
+        "output",
+        type=Path,
+        help="Path to output directory",
+    )
+
+    # Add option to force file over writting
+    parser.add_argument(
+        "-f",
+        "--force",
+        dest="force",
+        action="store_true",
+        default=False,
+        help="Force file over writting",
+    )
+    parser.add_argument(
+        "-g",
+        "--genomes",
+        type=Path,
+        default=None,
+        help="Path to dir containing already downloaded genomes",
+    )
+    # Add option to specific directory for log to be written out to
+    parser.add_argument(
+        "-l",
+        "--log",
+        type=Path,
+        metavar="log file name",
+        default=None,
+        help="Defines log file name and/or path",
+    )
+    # Add option to prevent over writing of existing files
+    # and cause addition of files to output directory
+    parser.add_argument(
+        "-n",
+        "--nodelete",
+        dest="nodelete",
+        action="store_true",
+        default=False,
+        help="enable/disable deletion of exisiting files",
+    )
+    # Add option to change the sample size (this is the number of CAZymes so the total sample size will be 2x sample_size)
+    parser.add_argument(
+        "-s",
+        "--sample_size",
+        type=int,
+        default=100,
+        help=(
+            "Number of CAZymes to be included in the sample size."
+            "Total sample size is twice this, becuase the test set "
+            "includes equal numbers of CAZymes and non-CAZymes"
+        ),
+    )
+    # Add option to specify verbose logging
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        default=False,
+        help="Set logger level to 'INFO'",
+    )
+
+    if argv is None:
+        # parse command-line
+        return parser
+    else:
+        # return namespace
+        return parser.parse_args(argv)
+
+
+def build_parser_db(argv: Optional[List] = None):
+    """Return ArgumentParser parser for script."""
+    # Create parser object
+    parser = argparse.ArgumentParser(
+        prog="create_test_set_from_db.py",
+        description="Build test sets for evaluating CAZyme prediction tools",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    # Add positional arguments to parser
+
+    parser.add_argument(
+        "email",
+        type=str,
+        help="User email address, required by NCBI to use Entrez",
+    )
+
+    parser.add_argument(
+        "yaml",
+        type=Path,
+        help="Path to yaml file containing genomic assembly accessions",
+    )
+
+    parser.add_argument(
+        "cazy",
+        type=Path,
+        help="Path to local CAZyme database",
     )
 
     parser.add_argument(
