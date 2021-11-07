13d3522910bff575aec80a26a952c1076cab9fef
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-11-07 14:22:21 +0000

    correct typo in fasta file path name

--- pyrewton/cazymes/evaluate_tools/stats/__init__.py
+++ pyrewton/cazymes/evaluate_tools/stats/__init__.py
@@ -136,7 +136,7 @@ def get_predictions(prediction_dir):
             continue
 
         # build path to the FASTA file
-        fasta_path = f"genbank_proteins_{tax_id}_{genomic_accession}._test_set.fasta"
+        fasta_path = f"genbank_proteins_{tax_id}_{genomic_accession}_test_set.fasta"
         fasta_path = output_dir / fasta_path
 
         # build dict of output paths
