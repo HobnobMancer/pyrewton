da25c42c45bfa12e07383546c558a92f841d9e18
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-11-09 17:55:48 +0000

    remove unused imports

--- pyrewton/cazymes/evaluate_tools/parse/parse_ecami_output.py
+++ pyrewton/cazymes/evaluate_tools/parse/parse_ecami_output.py
@@ -25,9 +25,7 @@
 import logging
 import re
 
-import pandas as pd
-import numpy as np
-
+from Bio import SeqIO
 from tqdm import tqdm
 
 from pyrewton.cazymes.evaluate_tools.parse import CazymeDomain, CazymeProteinPrediction
