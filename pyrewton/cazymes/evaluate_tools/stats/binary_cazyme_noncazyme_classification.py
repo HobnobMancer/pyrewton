aa92f102ad861eb0470160c3b94ab2d2a113602d
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-12-06 22:49:54 +0000

    import from cazy_webscraper

--- pyrewton/cazymes/evaluate_tools/stats/binary_cazyme_noncazyme_classification.py
+++ pyrewton/cazymes/evaluate_tools/stats/binary_cazyme_noncazyme_classification.py
@@ -27,7 +27,7 @@ import numpy as np
 
 from tqdm import tqdm
 
-from scraper.sql.sql_orm import Genbank, Session
+from cazy_webscraper.sql.sql_orm import Genbank, Session
 from sklearn.metrics import confusion_matrix, fbeta_score, precision_score, recall_score
 from sklearn.utils import resample
 
