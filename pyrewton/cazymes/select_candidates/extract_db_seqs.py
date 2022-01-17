9bf85a46e313b4ea722d882e473fac79878f172a
HobnobMancer <hobnobmancer@Paddington.localdomain>
2022-01-17 21:17:25 +0000

    update imports

--- pyrewton/cazymes/select_candidates/extract_db_seqs.py
+++ pyrewton/cazymes/select_candidates/extract_db_seqs.py
@@ -45,10 +45,15 @@ import logging
 
 from typing import List, Optional
 
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from saintBioutils.utilities.file_io import make_output_directory
 from saintBioutils.utilities.logger import config_logger
 from tqdm import tqdm
 
 from pyrewton.sql.sql_orm import get_cazome_db_connection
+from pyrewton.sql.sql_interface.load_data.load_genbank_data import get_protein_records
 from pyrewton.utilities.parsers.cmd_parser_extract_db_seq import build_parser
 
 
@@ -65,12 +70,57 @@ def main(argv: Optional[List[str]] = None, logger: Optional[logging.Logger] = No
         config_logger(args)
     logger = logging.getLogger(__package__)
 
+    if args.output is None:
+        output_path = "cazome_protein_seqs.fasta"
+    else:
+        if str(args.output.parent) != '.':
+            make_output_directory(args.output.parent, args.force, args.nodelete)
+        output_path = args.output
+
     connection = get_cazome_db_connection(args.database, args)
 
-    protein_records = get_protein_records(connection, args)
+    protein_accessions = get_user_protein_accessions(args)
 
-    for record in tqdm(protein_records, desc="Writing seqs to file"):
-        print('')
+    protein_records = get_protein_records(connection, protein_accessions)
+    
+    seq_records = []
+    
+    for protein_record in tqdm(protein_records, "Compiling SeqRecords"):
+        if protein_record.uniprot_id is None:
+            uniprot_id = "Unknown UniProt ID"
+        else:
+            uniprot_id = protein_record.uniprot_id
+        
+        new_record = SeqRecord(
+            Seq((protein_record.sequence).strip()),
+            id=protein_record.genbank_accession,
+            description=uniprot_id,
+        )
+        seq_records.append(new_record)
+
+    SeqIO.write(seq_records, output_path, "fasta")
+
+
+def get_user_protein_accessions(args):
+    """Retrieve protein accessions provided by the user in a txt file.
+    
+    :param args: cmd-line args parser
+    
+    Return list of unique protein accessions.
+    """
+    logger = logging.getLogger(__name__)
+
+    try:
+        with open(args.proteins, "r") as fh:
+            lines = fh.read().splitlines()
+    except FileNotFoundError:
+        logger.error(
+            f"Could not find file of protein accessions at {args.proteins}\nTerminating program"
+        )
+
+    protein_accessions = [line.strip() for line in lines]
+
+    return list(set(protein_accessions))
 
 
 if __name__ == "__main__":
