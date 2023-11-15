2b21149eac70c0dc3dc3d0a8f769e5ee2f771c4f
HobnobMancer <hobnobmancer@Paddington.localdomain>
2023-11-15 15:00:31 +0000

    add version and citation

--- pyrewton/__init__.py
+++ pyrewton/__init__.py
@@ -18,3 +18,7 @@
 # The MIT License
 """Python programme for identifying engineering candidates
 for advanced biocatalysis in biofuel production."""
+
+
+__version__ = "0.2.0"
+__citation__ = "Emma Hobbs. (2023). HobnobMancer/Pyrewton (v0.2.0). Zenodo. https://doi.org/10.5281/zenodo.3876218"
