82b2e534514e1ce6d83cd8699928bb265bb2ecca
HobnobMancer <hobnobmancer@Paddington.localdomain>
2023-11-20 16:27:09 +0000

    simplify parsing eCAMI
    combine annotations into dic

--- pyrewton/cazymes/annotate_cazome/tools/parse_tools/parse_ecami_output.py
+++ pyrewton/cazymes/annotate_cazome/tools/parse_tools/parse_ecami_output.py
@@ -59,8 +59,12 @@ def parse_ecami_output(txt_file_path, logger):
     :param text_file_path: path, path to the output text file
     :param logger: logger object
 
-    Return Pandas dataframe containing eCAMI output
+    Return dict
+    {toolname: {protein acc: {family: [domain range]}}}
+    or None if fails
     """
+    tool_predictions = {'eCAMI': {}}
+
     try:
         with open(txt_file_path, "r") as fh:
             ecami_file = fh.read().splitlines()
@@ -72,136 +76,23 @@ def parse_ecami_output(txt_file_path, logger):
         )
         return None
 
-    # build an empty dataframe to add predication outputs to
-    ecami_df = pd.DataFrame(columns=[
-        "protein_accession",
-        "cazy_family",
-        "cazy_subfamily",
-        "ec_number",
-        "additional_domains",
-    ])
-
     # parse the outputs so in format suitable for final dataframe
     for line in ecami_file:
         if line.startswith(">"):  # identifies new protein
             prediction_output = line.split("\t")
+            prot_acc = prediction_output[0]
 
-            # Retrieve the CAZy family
-            cazy_family = prediction_output[1].split(":")[0]
+            cazy_families = set()
+            for predictions in prediction_output[1:]:
+                domains = predictions.split("|")
+                for domain in domains:
+                    if domain.split(":")[0].startswith(("G", "P", "C", "A")):  # do not add EC numbers
+                        cazy_families.add(domain.split(":")[0])
 
-            # Check CAZy family is formated correctly
-            try:
-                re.match(r"\D{2,3}\d+", cazy_family.group())
-            except AttributeError:
-                logger.warning(
-                    f"Non-standardised CAZy family name for {line[0][1:]} {cazy_family} in\n"
-                    f"{txt_file_path}\n"
-                    f"Returning no predicted CAZy family for {line[0][1:]}"
-                )
-                cazy_family = np.nan
+            for cazy_family in cazy_families:
+                try:
+                    tool_predictions['eCAMI'][prot_acc][cazy_family] = [None]
+                except KeyError:
+                    tool_predictions['eCAMI'][prot_acc] = {cazy_family: None}
 
-            # Retrieve subfamily, additional domains and EC numbers from the additional info
-            additional_domains, ec_number, cazy_subfam = get_ecami_additional_info(
-                prediction_output,
-                cazy_family,
-                txt_file_path,
-                logger,
-            )
-
-            # build dict to enable easy building of df
-            prediction = {
-                "protein_accession": [prediction_output[0][1:]],
-                "cazy_family": [cazy_family],
-                "cazy_subfamily": [cazy_subfam],
-                "ec_number": [ec_number],
-                "additional_domains": [additional_domains],
-            }
-
-            prediction_df = pd.DataFrame(prediction)
-            ecami_df = ecami_df.append(prediction_df)
-
-    return ecami_df
-
-
-def get_ecami_additional_info(prediction_output, cazy_family, txt_file_path, logger):
-    """Retrieve additional predicated domains and EC numbers from eCAMI output file.
-
-    :param prediction_output: list of predicted items for a given protein
-    :param cazy_family: str, predicted CAZy family
-
-    Return list of additional domains and list of EC numbers as strings, items separated by ', '.
-    """
-    additional_domains = []
-    ec_numbers = []
-    subfams = []
-
-    # separate the data stored in the 'additional' data section of the line in the eCAMI output file
-    additional_info = prediction_output[2].split("|")
-
-    for item in additional_info:
-        item = item.split(":")[0]  # drop the eCAMI group number
-        item = item.strip()
-
-        # check if it is an EC number
-        if item.find(".") != -1:
-            ec_numbers.append(item)
-            continue
-
-        # check if it is a subfamily or family prediction
-        try:
-            re.match(r"(\D{2,3}\d+)|(\D{2,3}\d+_\d+)", item).group()
-        except AttributeError:
-            logger.warning(
-                f"Non-standardised output for {prediction_output[0][1:]} {item} in\n"
-                f"{txt_file_path}"
-                "Returning no CAZy family/subfamily"
-            )
-            continue
-
-        # Check if subfamily
-        if item.find("_") != -1:  # predicated CAZy subfamily
-            # check format
-            try:
-                re.match(r"\D{2,3}\d+_\d+", item).group()
-                # check if it is the subfamily of the predicated CAZy family
-                if cazy_family == item[:item.find("_")]:
-                    subfams.append(item)
-                else:
-                    additional_domains.append(item)
-            except AttributeError:
-                logger.warning(
-                    "Non-standardised CAZy subfamily name for "
-                    f"{prediction_output[0][1:]} {item} in\n"
-                    f"{txt_file_path}"
-                    "Returning subfamily"
-                )
-
-        else:  # predicated CAZy family
-            # check format
-            try:
-                re.match(r"\D{2,3}\d+", item).group()
-                additional_domains.append(item)
-            except AttributeError:
-                logger.warning(
-                    f"Non-standardised CAZy family name for {prediction_output[0][1:]} {item} in\n"
-                    f"{txt_file_path}"
-                    "Returning no CAZy family"
-                )
-
-    # convert empty lists to null values if necessary, or make list more human readable
-    if len(additional_domains) == 0:
-        additional_domains = np.nan
-    else:
-        additional_domains = ", ".join(additional_domains)
-
-    if len(ec_numbers) == 0:
-        ec_numbers = np.nan
-    else:
-        ec_numbers = ", ".join(ec_numbers)
-
-    if len(subfams) == 0:
-        subfams = np.nan
-    else:
-        subfams = ", ".join(subfams)
-
-    return(additional_domains, ec_numbers, subfams)
+    return tool_predictions
